8067bf74bfb501c4d34c5c33f6d33d2a538c2335
WK <741341649@qq.com>
2020-03-13 21:22:59 +0800

    创建 Icon.vue 组件引入svg

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,23 @@
 <template>
     <nav>
-        <router-link to="/money">记账</router-link>
-        <router-link to="/labels">标签</router-link>
-        <router-link to="statistics">统计</router-link>
+        <router-link to="/money">
+            <Icon name="money"/>
+            记账
+        </router-link>
+        <router-link to="/labels">
+            <Icon name="label"/>
+            标签
+        </router-link>
+        <router-link to="statistics">
+            <Icon name="statistics"/>
+            统计
+        </router-link>
     </nav>
 </template>
 
 <script lang="ts">
+
+
     import Vue from 'vue';
     import {Component} from 'vue-property-decorator';
 
@@ -17,7 +28,7 @@
 </script>
 
 <style lang="scss" scoped>
-    nav{
+    nav {
         display: flex;
         flex-direction: row;
         justify-content: space-between;
