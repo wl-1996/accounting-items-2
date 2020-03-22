3bf3b91bc491d01a8fff21b414832e24fd4e86aa
WK <741341649@qq.com>
2020-03-22 11:22:05 +0800

    在Money.vue里改用Tabs.vue组件，删除Types.vue组件的引用

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -8,7 +8,7 @@
                       placeholder="在这里添加备注"
                       @update:value="onUpdateNotes"/>
         </div>
-        <Types :value.sync="record.type"/>
+        <Tabs :data-source="typeList" :value.sync="record.type"/>
         <NumberPad @submit="saveRecord"
                    :value.sync="record.amount"/>
     </Layout>
@@ -19,13 +19,17 @@
     import {Component} from 'vue-property-decorator';
     import Tags from '@/components/Money/Tags.vue';
     import FormItem from '@/components/Money/FormItem.vue';
-    import Types from '@/components/Money/Types.vue';
     import NumberPad from '@/components/Money/NumberPad.vue';
+    import Tabs from '@/components/Tabs.vue';
 
     @Component({
-        components: {NumberPad, Types, FormItem, Tags},
+        components: {Tabs, NumberPad, FormItem, Tags},
     })
     export default class Money extends Vue {
+        typeList = [{text: '支出', value: '-'},
+            {text: '收入', value: '+'}
+        ];
+
         beforeCreate() {
             this.$store.commit('fetchRecords');
         }
